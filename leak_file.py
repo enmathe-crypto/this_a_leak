7513923533c727cfec31be50b42cba27d390a774
enmathe-crypto <enzomathe+core_alerting+test@gmail.com>
2021-03-17 12:20:14 +0100

    Update leak_file.py

--- leak_file.py
+++ leak_file.py
@@ -10,3 +10,7 @@ antigravity.fly()
 
 var mongo_uri = "mongodb+srv://testuser:hub24aoeu@gg-is-awesome-gg273.mongodb.net/test?retryWrites=true&w=majority";
 
+var mongo_uri = "mongodb+srv://testuser:enz24aoeu@gg-is-awesome-gg273.mongodb.net/test?retryWrites=true&w=majority";
+
+var mongo_uri = "mongodb+srv://testuser:tmz24aoeu@gg-is-awesome-gg273.mongodb.net/test?retryWrites=true&w=majority";
+
